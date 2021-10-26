467ccb3a8112d9cbc77dbec2bb6ab90f8b68a7e1
hungngocphat01 <hungngocphat01@gmail.com>
2021-10-26 22:57:43 +0700

    Add mean, std, ...

--- honolib/Series.py
+++ honolib/Series.py
@@ -1,3 +1,4 @@
+import math 
 from honolib.utils import detect_type_value
 
 class Series:
@@ -17,7 +18,7 @@ class Series:
 
     def __init_from_list(self, obj):
         self.rows = obj
-        self.__process_empty_cells(operation='read')
+        self._process_empty_cells(operation='read')
         self.__process_col_type()
 
     def __process_col_type(self):
@@ -39,11 +40,12 @@ class Series:
             self.rows[index] = t(row)
 
 
-    def __process_empty_cells(self, operation='read'):
+    def _process_empty_cells(self, operation='read'):
         """
         Hàm xử lý các hàng rỗng
         - Nếu đang đọc từ CSV: gán tất cả hàng rỗng thành None
         - Nếu đang ghi xuống CSV: gán tất cả hàng None thành chuỗi rỗng
+        operation = ['read' | 'write']
         """
         assert operation in ('read', 'write'), 'Operation not supported'
 
@@ -52,6 +54,9 @@ class Series:
                 self.rows[index] = None
             elif operation == 'write' and row is None:
                 self.rows[index] = ''
+    
+    def __len__(self):
+        return len(self.rows)
     
     def cast(self, dtype: type):
         """
@@ -64,3 +69,176 @@ class Series:
             except:
                 self.rows[index] = None
         self.dtype = dtype
+    
+    def count_na(self):
+        """
+        Đếm số hàng bị thiếu trong cột
+        """
+        count = 0
+        for row in self.rows:
+            if row is None: 
+                count += 1
+        return count
+
+    def count_non_na(self):
+        """
+        Đếm số hàng không bị thiếu trong cột
+        """
+        return len(self.rows) - self.count_na()
+    
+    def sum(self):
+        """
+        Tính tổng cột
+        """
+        if self.dtype not in [int, float]:
+            return None 
+        
+        s = 0
+        for row in self.rows:
+            if row is not None:
+                s += row 
+        return s 
+    
+    def mean(self):
+        """
+        Tính giá trị trung bình của cột
+        """
+        if self.dtype not in [int, float]:
+            return None 
+
+        n = self.count_non_na()
+        s = self.sum()
+        return s/n
+    
+    def std(self):
+        """
+        Tính độ lệch chuẩn của cột
+        """
+        if self.dtype not in [int, float]:
+            return None 
+
+        n = self.count_non_na()
+        m = self.mean()
+        s = 0
+        for row in self.rows:
+            if row is None:
+                continue
+            s += (row - m) ** 2
+        return math.sqrt(s / n)
+    
+    def frequency_table(self):
+        """
+        Lập bảng tần suất các giá trị phân biệt trong bảng (bao gồm None)
+        """
+        frequency_table = {}
+        for row in self.rows:
+            if row in frequency_table:
+                frequency_table[row] += 1 
+            else: 
+                frequency_table[row] = 1
+        return frequency_table
+    
+    def mode(self):
+        """
+        Tính giá trị mode của cột. Nếu có nhiều mode chỉ lấy giá trị đầu tiên.
+        Trả về: mode_value, frequency
+        """
+        frequency_table = self.frequency_table()
+        max_freq = 0
+        max_value = 0
+        for (value, freq) in frequency_table.items():
+            if value is not None:
+                if freq > max_freq:
+                    max_freq = freq 
+                    max_value = value 
+        return max_value, max_freq
+    
+    def minmax(self):
+        """
+        Hàm tính giá trị min và max của cột
+        """
+        if self.dtype not in [int, float]:
+            return None 
+
+        min_value = +math.inf
+        max_value = -math.inf
+
+        for row in self.rows: 
+            if row is None:
+                continue
+            if row < min_value:
+                min_value = row 
+            if row > max_value: 
+                max_value = row 
+        return min_value, max_value
+    
+    def median(self):
+        """
+        Hàm tính giá trị trung vị của cột
+        """
+        if self.dtype not in [int, float]:
+            return None 
+
+        n = len(self.rows)
+        m = math.floor((n + 1) / 2)
+
+        filtered_rows = list(filter(lambda x: x is not None, self.rows))
+        sorted_rows = sorted(filtered_rows)
+
+        return sorted_rows[m]
+    
+    def describe(self):
+        """
+        Hàm in ra thông tin mô tả của cột
+        """
+        rnd = lambda x: round(x, 4) if x is not None else None
+
+        print('Label:', self.label)
+        print('Length:', len(self.rows))
+        print('Null values:', self.count_na())
+        print('Sum:', rnd(self.sum()))
+        print('Mean:', rnd(self.mean()))
+        print('Median:', rnd(self.median()))
+        print('Std:', rnd(self.std()))
+        print('Min-Max', self.minmax())
+        print('Mode:', self.mode())
+    
+    def fill_na(self, method='mean'):
+        """
+        Điền giá trị rỗng cho cột
+        method = ['mean' | 'med' | 'mode']
+        """
+        assert method in ['mean', 'med', 'mode'], 'Unsupported fill method'
+        
+        if method == 'mean':
+            fill_value = self.mean()
+        elif method == 'med':
+            fill_value = self.median()
+        elif method == 'mode':
+            fill_value, _ = self.mode()
+        
+        for i, row in enumerate(self.rows):
+            if row is None:
+                self.rows[i] = fill_value
+    
+    def normalize(self, method='zscore'):
+        """
+        Chuẩn hóa cột
+        method = ['zscore' | 'zscore']
+        """
+        assert method in ['zscore', 'minmax'], 'Invalid normalize method'
+        
+        if method == 'zscore':
+            mean = self.mean()
+            std = self.std()
+        elif method == 'minmax':
+            min_value, max_value = self.minmax()
+
+
+        for i, row in enumerate(self.rows):
+            if row is None:
+                continue
+            if method == 'zscore':
+                self.rows[i] = (row - mean) / std
+            elif method == 'minmax':
+                self.rows[i] = (row - min_value) / (max_value - min_value)
